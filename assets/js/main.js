ef50bb719f6aece5d39f442490f95272895da4f7
Ed Hagerty <ed@hagerty.net>
2021-05-28 15:22:31 +0100

    files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,39 @@
-particlesJS.load('particles-js', 'assets/particles.json', function () {
-    console.log('callback - particles.js config loaded');
+particlesJS.load("particles-js", "assets/particles.json", function () {
+  console.log("callback - particles.js config loaded");
 });
-const instance = new Typewriter('#typewriter', {
-    loop: true
+const instance = new Typewriter("#typewriter", {
+  loop: true,
 });
 
-instance.typeString("Hello World!")
-    .pauseFor(1000)
-    .deleteAll()
-    .typeString('I am Ed Hagerty.')
-    .pauseFor(1000)
-    .deleteChars(15)
-    .typeString('an aspiring<br>master of the universe.')
-    .pauseFor(1000)
-    .deleteChars(29)
-    .typeString('currently learning<br>your bank account details.')
-    .pauseFor(1000)
-    .deleteAll()
-    .typeString('see how smart I am at <br>github.com/ehagerty')
-    .pauseFor(1000)
-    .deleteAll()
-    .start();
+instance
+  .typeString("You like Kung Fu?")
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("what if what goes around actually just keeps on going?")
+  .pauseFor(1000)
+  .deleteChars(15)
+  .typeString("what if what goes up started going sideways?")
+  .pauseFor(1000)
+  .deleteChars(29)
+  .typeString("what would Kirk do?")
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("when everything is urgent, nothing is")
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("when anything is optional, everything is")
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("pizza, with a side order of justice")
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("find one in every car. You'll see")
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("there's one about an enormous fish which you haven't heard")
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("it's like being jealous of a bag of sand")
+  .pauseFor(1000)
+  .deleteAll()
+  .start();
